0703be952d9ea31e6022296882e0e306f9ad5df7
yhf <youssifhany75@gmail.com>
2023-04-29 03:37:26 +0200

    update: app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { NotFoundComponent } from './layout/components/not-found/not-found.component';
+import { LoginComponent } from './features/auth/pages/login/login.component';
+import { disableAuthGuard } from './shared/guards/disable-auth.guard';
+import { authGuard } from './shared/guards/auth.guard';
+import { HomeComponent } from './features/home/pages/home.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    canActivate: [authGuard],
+  },
+  { path: 'home', redirectTo: '', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canMatch: [disableAuthGuard],
+  },
+  // Not found page
+  { path: '404', component: NotFoundComponent, pathMatch: 'full' },
+  { path: '**', redirectTo: '404' },
+];
