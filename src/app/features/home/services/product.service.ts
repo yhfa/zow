115ec31213dfb621b8d68902930a75cc9a3cb630
yhf <youssifhany75@gmail.com>
2023-04-29 06:14:17 +0200

    add: product service

---
+++ src/app/features/home/services/product.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import {
+  BehaviorSubject,
+  combineLatest,
+  filter,
+  forkJoin,
+  map,
+  switchMap,
+  tap,
+} from 'rxjs';
+
+import { Product } from '../models/product';
+import { CategoryService } from './category.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  private _productsUrl = 'https://fakestoreapi.com/products';
+
+  constructor(private _http: HttpClient) {}
+
+  private _isProductsLoading = new BehaviorSubject(false);
+  isProductsLoading$ = this._isProductsLoading.asObservable();
+
+  private _selectedCategorySubject = new BehaviorSubject<string | null>(null);
+  selectedCategoryAction$ = this._selectedCategorySubject.asObservable();
+
+  products$ = this.selectedCategoryAction$.pipe(
+    filter(selectedCategory => Boolean(selectedCategory)),
+    switchMap(selectedCategory => {
+      this._isProductsLoading.next(true);
+
+      return this._http.get<Product[]>(
+        `${this._productsUrl}/category/${selectedCategory}`
+      );
+    }),
+    tap(data => {
+      this._isProductsLoading.next(false);
+      console.log('Products: ', JSON.stringify(data));
+    })
+  );
+
+  selectedCategoryChange(selectedCategory: string): void {
+    this._selectedCategorySubject.next(selectedCategory);
+  }
+}
