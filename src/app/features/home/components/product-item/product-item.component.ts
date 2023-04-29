16b905b8bfd921baa8cea7f2a8385c6a1f434f22
yhf <youssifhany75@gmail.com>
2023-04-29 11:56:05 +0200

    update: adjust responsive style

--- src/app/features/home/components/product-item/product-item.component.ts
+++ src/app/features/home/components/product-item/product-item.component.ts
@@ -1,16 +1,24 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import { StarRatingModule } from 'angular-star-rating';
 
 import { Product } from '../../models/product';
+import { LocaleService } from 'src/app/shared/services/locale.service';
+import { Language } from 'src/app/shared/interfaces';
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, MatTooltipModule, StarRatingModule],
   templateUrl: './product-item.component.html',
   styleUrls: ['./product-item.component.scss'],
 })
 export class ProductItemComponent {
   @Input() product!: Product;
+  Language = Language;
+
+  constructor(public localeService: LocaleService) {}
 }
