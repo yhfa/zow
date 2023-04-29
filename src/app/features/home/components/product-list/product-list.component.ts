f22fa85a6312490acae07c09447f94a0154f7ddb
yhf <youssifhany75@gmail.com>
2023-04-29 06:13:56 +0200

    add: product-list component

---
+++ src/app/features/home/components/product-list/product-list.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatTabChangeEvent, MatTabsModule } from '@angular/material/tabs';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { combineLatest, map } from 'rxjs';
+
+import { ProductService } from '../../services/product.service';
+import { CategoryService } from '../../services/category.service';
+import { ProductItemComponent } from '../../components/product-item/product-item.component';
+
+@Component({
+  selector: 'app-product-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTabsModule,
+    MatProgressSpinnerModule,
+    ProductItemComponent,
+  ],
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.scss'],
+})
+export class ProductListComponent {
+  constructor(
+    private _productsService: ProductService,
+    private _categoryService: CategoryService
+  ) {}
+
+  products$ = this._productsService.products$;
+  isLoading$ = this._productsService.isProductsLoading$;
+  categories$ = this._categoryService.categories$;
+
+  vm$ = combineLatest([this.products$, this.isLoading$, this.categories$]).pipe(
+    map(([products, isLoading, categories]) => ({
+      products,
+      isLoading,
+      categories,
+    }))
+  );
+
+  onSelected({ tab }: MatTabChangeEvent): void {
+    this._productsService.selectedCategoryChange(tab.textLabel);
+  }
+}
