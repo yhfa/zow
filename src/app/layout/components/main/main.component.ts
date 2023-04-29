ee7c35e0271bfeba5273a541d4c917fc1c697cfe
yhf <youssifhany75@gmail.com>
2023-04-29 03:40:26 +0200

    add: main component

---
+++ src/app/layout/components/main/main.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-main',
+  standalone: true,
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss'],
+  imports: [CommonModule, RouterModule, MatSidenavModule, NavbarComponent],
+})
+export class MainComponent {}
