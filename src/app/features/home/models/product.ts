065ea1f8ed47fc7210781ae7892320a2ec14fd44
yhf <youssifhany75@gmail.com>
2023-04-29 09:54:38 +0200

    update: make rating & id optional on product

--- src/app/features/home/models/product.ts
+++ src/app/features/home/models/product.ts
@@ -1,11 +1,11 @@
 export interface Product {
-  id: number;
+  id?: number;
   title: string;
   price: number;
   description: string;
   category: string;
   image: string;
-  rating: {
+  rating?: {
     rate: number;
     count: number;
   };
